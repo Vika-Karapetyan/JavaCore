85797ca2a34fe75a5f8e9f028d9858a8b9ef57c9
Vika Karapetyan <vikakarapetyan08@gmail.com>
2023-09-02 22:26:08 +0400

    homework2

--- src/homework/ForExample.java
+++ src/homework/ForExample.java
@@ -3,7 +3,10 @@ package homework;
 public class ForExample {
     public static void main(String[] args) {
         for (int i = 0; i <= 1000; i++) {
-            System.out.print(i + "-");
+            System.out.print(i);
+            if(i<1000){
+                System.out.print("-");
+            }
         }
         System.out.println();
 
