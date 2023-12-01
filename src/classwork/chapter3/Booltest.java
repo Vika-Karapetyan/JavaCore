229b9f0125b17016b7fef4ef699374893eb4a2d1
Vika Karapetyan <vikakarapetyan08@gmail.com>
2023-12-02 01:47:36 +0400

    just

---
+++ src/classwork/chapter3/Booltest.java
@@ -0,0 +1,16 @@
+package classwork.chapter3;
+
+public class Booltest {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b равно " + b);
+        b = true;
+        System.out.println("b равно " + b);
+        if (b) System.out.println("Этoт код выполняется.");
+        b = false;
+        if (b) System.out.println("Этoт код не выполняется.");
+        System.out.println("10 > 9 равно " + (10 > 9));
+
+    }
+}
---
+++ src/classwork/chapter5/DoWhile.java
@@ -0,0 +1,13 @@
+package classwork.chapter5;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i=0;
+        int n=20;
+        do{
+            System.out.println("Hello");
+            i++;
+        }
+        while(i<n);
+    }
+}
